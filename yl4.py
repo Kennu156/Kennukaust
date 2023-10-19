33783b6e188a86bf474a81f6f0c2c5aeeea2e0fb
Kennu156 <kenmarttipaju@gmail.com>
2023-10-19 11:50:10 +0300

    commit message

---
+++ yl4.py
@@ -0,0 +1,7 @@
+num1 = int(input('Sisesta number:-'))
+num2 = int(input('Sisesta number:-'))
+
+if(num1<=num2):
+    print(num1, 'on väiksem')
+else:
+    print(num2, 'on väiksem')
---
+++ yl7.py
@@ -0,0 +1,7 @@
+n = int(input('Sisesta number:-'))
+Paaris = n % 2
+
+if(Paaris == 0):
+    print('number on paaris')
+else:
+    print('number on paaritu')    
