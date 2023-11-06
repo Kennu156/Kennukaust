dd6ac64ea3965679c587fb9d4156a0194c856c8f
Kennu156 <kenmarttipaju@gmail.com>
2023-11-06 14:15:15 +0200

    commit

--- yl11.py
+++ yl11.py
@@ -1,10 +1,12 @@
 a = input('Sisesta string millel on vähemalt 7 sümbolit ja paarituarvuline: ')
 a = a.strip()
 print(a)
+center = len(a) / 2
 
-if len(a) < 7 and len(a) % 2:
+
+if  len(a) % 2 and len(a) > 7:
+    print(a[center-1:center+2])
+elif len(a) < 7:
     print('ei')
-elif(len(a) > 7):
-    print('ok')
 else:
-    print('ok')
+    print(a)
---
+++ yl18.py
@@ -0,0 +1,3 @@
+a = range(1, 6, 4)
+for x in reversed(a):
+    print(x)
