4ff8996b04e0ff5495303b895662b89edfffccf2
Kenmartti <145542503+Kennu156@users.noreply.github.com>
2024-01-11 14:17:48 +0200

    Update yl27.py

--- yl27.py
+++ yl27.py
@@ -10,3 +10,41 @@ reddit = praw.Reddit(
 
 for submission in reddit.subreddit("Eesti").top(limit=10):
     print(submission.title)
+
+import praw
+
+reddit = praw.Reddit(
+   client_id= 'tmNtAvAgBjdEatlWk2PnWw',
+  client_secret= 'NGaJwqso_109dVX3tlMIE-Z1aVBSAw',
+    user_agent="python3:Kennu156:0.1 (by u/Kennu156)",
+)
+subreddit = reddit.subreddit('Eesti')
+
+words = []
+
+for submission in subreddit.hot(limit = 2):
+    submission.comments.replace_more(limit=0)
+    for top_level_comment in submission.comments:
+        word = ""
+        for letter in top_level_comment.body:
+            if(letter == " "):
+                #if(word and not word[-1].isalnum()):
+                 #   word = word[:-1]
+                #if not word in commonWords:
+                words.append(word)
+                word = ''
+            else:
+                word += letter
+        
+wordCount = {}
+
+for word in words:
+    if word in wordCount:
+        wordCount[word] += 1
+    else:
+        wordCount[word] = 1
+    
+
+print(wordCount)
+    
+        
